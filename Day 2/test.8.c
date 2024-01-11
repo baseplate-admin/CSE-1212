47e40d9e79d9a57d7bb12957fb13a97a715c840f
zarif-ahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2024-01-11 10:58:11 -0800

    Update test.8.c

--- Day 2/test.8.c
+++ Day 2/test.8.c
@@ -5,7 +5,7 @@ void main(){
 
     float diameter;
 
-    printf("Enter the diameter of a cirlce\n");
+    printf("Enter the diameter of a circle:\n");
     scanf("%f",&diameter);
     printf("%f\n",(diameter/2)*(diameter/2)*3.1416);
 }
