768eda442c52329dcc16526a507587c5838f2e6a
zarif-ahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2024-01-11 10:18:52 -0800

    Update test.7.c

--- Day 2/test.7.c
+++ Day 2/test.7.c
@@ -3,7 +3,7 @@
 
 void main(){
     float r;
-    printf("Print the radius of the circle.\n");
+    printf("Enter the radius of the circle.\n");
     scanf("%f",&r);
 
     printf("%f\n",3.1416*(r*r));
